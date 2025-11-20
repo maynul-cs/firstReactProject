23fa7c4eb04468a7801deae12f3756a202266a39
maynul-cs <maynulislammishu@gmail.com>
2025-11-21 02:44:18 +0600

    Create Register Page & Improve Login Page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,42 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='flex items-center justify-center h-[80vh] px-4 bg-green-900'>
-      <div className='text-white bg-emerald-400 max-w-sm p-8'>
+    <div className='flex items-center justify-center h-[90vh] px-4 bg-green-900'>
+      <div className=' bg-white max-w-sm p-8'>
         <h2 className='text-green-800 font-bold text-center text-2xl mb-6'> Login </h2>
         <form className='space-y-4'>
            <input 
            type="email" 
            name=''
-           placeholder='Enter a email'
-           className='w-full border px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           placeholder='Enter a valid Email'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
            id='' />
 
            <input 
            type="password" 
            name=''
-           placeholder='Enter a password'
-           className='w-full border px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           placeholder='Enter a Password'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
            id='' />
 
            <button type='submit'
-           className='w-full bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer'>
+           className='w-full mb-2 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer'>
               Login
            </button>
+           <p className='text-sm text-right text-gray-600 hover:underline cursor-pointer'>
+            Forgot Password?
+           </p>
         </form>
-        <div className='flex flex-col space-y-3 mt-3'>
-          <button className='bg-red-600 py-2 rounded hover:bg-red-700 cursor-pointer'> 
-            Login with Google 
+
+        {/* Divider here */}
+        <div className='flex items-center my-6'>
+          <div className='flex-grow h-px bg-gray-700'></div >
+            <span className='mx-4 text-gray-500 text-sm'> OR </span>
+          <div className='flex-grow h-px bg-gray-700'></div >
+        </div>
+        <div className='flex flex-col space-y-3 mt-6'>
+          <button className=  'text-white bg-red-600 py-2 rounded hover:bg-red-700 cursor-pointer flex items-center justify-center gap-2'> 
+            Login with Google <FaGoogle />
           </button>
-          <button className='bg-gray-700 py-2 rounded hover:bg-gray-900 cursor-pointer'>
-            Login with GitHub
+          <button className='text-white bg-gray-700 py-2 rounded hover:bg-gray-900 cursor-pointer flex items-center justify-center gap-2'>
+            Login with GitHub <FaGithub />
           </button>
         </div>
-        <div className='text-center mt-4 text-white'>
-          <p>
-            Don't have an account? <Link to={'/register'} className='text-blue-700 underline'> Register </Link>
+        <div className='text-center mt-4'>
+          <p className='text-sm'>
+            Don't have an account? <Link to={'/register'} className='text-blue-700 font-semibold hover:underline'> Register </Link>
           </p>
         </div>
       </div>
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='flex items-center justify-center h-[100vh] px-4 bg-green-900'>
+      <div className=' bg-white max-w-lg p-8'>
+        <h2 className='text-green-800 font-bold text-center text-2xl mb-6'> Register </h2>
+        <form className='space-y-4'>
+           <input 
+           type="text" 
+           name=''
+           placeholder='Full Name'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           id='' />
+
+           <input 
+           type="email" 
+           name=''
+           placeholder='Email'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           id='' />
+
+           <input 
+           type="password" 
+           name=''
+           placeholder='Password'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           id='' />
+
+           <input 
+           type="password" 
+           name=''
+           placeholder='Confirm Password'
+           className='w-full border focus:outline-none px-4 py-2 rounded focus:ring-2 focus:ring-green-500'
+           id='' />
+
+           <button type='submit'
+           className='w-full mb-2 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 cursor-pointer'>
+              Sign Up
+           </button>
+           <div className='flex itmes-center justify-between'>
+            <p>
+              <input 
+              type="checkbox"
+              name=''
+              id='checkBox'
+               />
+               <label htmlFor="checkBox"> Accept Terms & <Link className='text-blue-700'> Conditions </Link> </label>
+            </p>
+            <p className='text-sm text-right text-gray-600 hover:underline cursor-pointer'>
+            Forgot Password?
+           </p>
+           </div>
+        </form>
+
+        {/* Divider here */}
+        <div className='flex items-center my-6'>
+          <div className='flex-grow h-px bg-gray-700'></div >
+            <span className='mx-4 text-gray-500 text-sm'> OR </span>
+          <div className='flex-grow h-px bg-gray-700'></div >
+        </div>
+
+        <div className='flex flex-col space-y-3 mt-6'>
+          <button className='text-white bg-red-600 py-2 rounded hover:bg-red-700 cursor-pointer flex items-center justify-center gap-2'> 
+            Register with Google <FaGoogle />
+          </button>
+          <button className='text-white bg-gray-700 py-2 rounded hover:bg-gray-900 cursor-pointer flex items-center justify-center gap-2'>
+            Register with GitHub <FaGithub />
+          </button>
+        </div>
+        <div className='text-center mt-4'>
+          <p className='text-sm'>
+            Already have an account? <Link to={'/login'} className='text-blue-700 font-semibold hover:underline'> Login Here </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
